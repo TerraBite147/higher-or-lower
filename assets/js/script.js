287b6069d79884439dff8480282158d530aaf23d
RLCarlow <logan.carlow@gmail.com>
2023-09-11 18:04:37 +0100

    feat(index): Add close button to rules modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 const modal = document.querySelector('#rules-modal')
 const openRules = document.querySelector('#rules');
+const closeRules = document.querySelector('#exit-rules');
 
 openRules.addEventListener('click', () => {
     modal.showModal();
+})
+closeRules.addEventListener('click', () => {
+    modal.close();
 })
