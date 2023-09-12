e6c5605d96fc53fb596198cc8322a3ddb7b3f38f
RLCarlow <logan.carlow@gmail.com>
2023-09-12 17:33:23 +0100

    feat(script): Add createDeck function to create a 52 card deck

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,38 @@ if (winnerModal) {
     winnerModal.close();
   });
 }
+
+let cards = [];
+/**
+ * Creates an ordered object array of 52 cards with value pairs name: and value: 
+ */
+function createDeck() {
+  cards = [];
+  let suits = ["spades", "hearts", "diamonds", "clubs"];
+  let values = [
+    "ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "jack",
+    "queen",
+    "king",
+  ];
+
+  for (const suit of suits) {
+    for (const value of values) {
+      const card = {
+        name: `${value}_of_${suit}`,
+        value: values.indexOf(value) + 1, // Assign values from 1 to 13
+      };
+      cards.push(card);
+    }
+  }
+  console.log(cards)
+}
