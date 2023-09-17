a3ec6e2f2b26d5e91e808efde39abe8e60c9a8ce
RLCarlow <logan.carlow@gmail.com>
2023-09-17 14:44:25 +0100

    feat(script): Adjust code to follow more logical structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,167 +1,168 @@
-// Rules Modal open and close will only create event listeners if content is on page.
-const rulesModal = document.querySelector("#rules-modal");
-if (rulesModal) {
-  const openRules = document.querySelector("#rules");
-  const closeRules = document.querySelector("#exit-rules");
-  openRules.addEventListener("click", () => {
-    rulesModal.showModal();
-  });
-  closeRules.addEventListener("click", () => {
-    rulesModal.close();
-  });
-}
-// Winner Modal open and close will only create event listeners if content is on page.
-const winnerModal = document.querySelector("#winner-modal");
-if (winnerModal) {
-  const closeWinner = document.querySelector("#exit-winner");
-  closeWinner.addEventListener("click", () => {
-    winnerModal.close();
-  });
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-  if (cardsContainer) {
-    initializeGame();
+  // Global Variables
+  const rulesModal = document.querySelector("#rules-modal");
+  const winnerModal = document.querySelector("#winner-modal");
+  const cardsContainer = document.getElementById("cards-container");
+  const currentCard = document.getElementById("current-card");
+  const lower = document.querySelector("#lower-btn");
+  const bankButton = document.querySelector("#bank-btn");
+
+  let bankedPointsElement = document.querySelector("#banked-pts");
+  let streakPointsElement = document.querySelector("#streak-pts");
+
+  let cards = [];
+  let currentCardValue = null;
+  let previousCardValue = null;
+  // Find User's choice
+  let userChoice = null;
+
+  // Run code for Index.html
+  if (rulesModal) {
+    const openRules = document.querySelector("#rules");
+    const closeRules = document.querySelector("#exit-rules");
+    openRules.addEventListener("click", () => {
+      rulesModal.showModal();
+    });
+    closeRules.addEventListener("click", () => {
+      rulesModal.close();
+    });
   }
-});
 
-function initializeGame() {
-  createDeck();
-  drawCard();
-}
-
-// Card array to hold all 52 cards
-let cards = [];
-
-/**
- * Creates an ordered object array of 52 cards with value pairs name: and value:
- */
-function createDeck() {
-  cards = [];
-  let suits = ["spades", "hearts", "diamonds", "clubs"];
-  let values = [
-    "ace",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "jack",
-    "queen",
-    "king",
-  ];
-
-  for (const suit of suits) {
-    for (const value of values) {
-      const card = {
-        name: `${value}_of_${suit}`,
-        value: values.indexOf(value) + 1, // Assign values from 1 to 13
-      };
-      cards.push(card);
+  // Run code for game.html
+  if (winnerModal) {
+    let streakPoints = parseInt(streakPointsElement.innerHTML);
+    let bankedPoints = parseInt(bankedPointsElement.innerHTML);
+    const closeWinner = document.querySelector("#exit-winner");
+    closeWinner.addEventListener("click", () => {
+      winnerModal.close();
+      initializeGame()
+    });
+    if (cardsContainer) {
+      initializeGame();
     }
-  }
-}
 
-// Variables for
-const cardsContainer = document.getElementById("cards-container");
-const currentCard = document.getElementById("current-card");
-let currentCardValue = null;
-let previousCardValue = null;
+    /**
+     * Creates an ordered object array of 52 cards with value pairs name: and value:
+     */
+    function createDeck() {
+      cards = [];
+      let suits = ["spades", "hearts", "diamonds", "clubs"];
+      let values = [
+        "ace",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "jack",
+        "queen",
+        "king",
+      ];
 
-/**
- * Displays a random card from the cards array in the card-container div
- *
- */
-function drawCard() {
-  previousCardValue = currentCardValue;
-  const randomIndex = Math.floor(Math.random() * cards.length);
-  const selectedCard = cards[randomIndex];
-
-  // Create the filename for the card image
-  const cardImageName = `${selectedCard.name}.svg`;
-
-  // Set the source of the current card element to display the selected card
-  currentCard.src = `assets/images/cards/SVG-cards-1.3/${cardImageName}`;
-  currentCard.alt = `${cardImageName}`;
-  currentCard.setAttribute("aria-label", cardImageName);
-  currentCardValue = selectedCard.value;
-}
-
-// Find User's choice
-let userChoice = null;
-
-const higher = document.querySelector("#higher-btn");
-higher.addEventListener("click", () => {
-  drawCard();
-  userChoice = "Higher";
-  compareUserChoice(userChoice);
-});
-
-const lower = document.querySelector("#lower-btn");
-lower.addEventListener("click", () => {
-  drawCard();
-  userChoice = "Lower";
-  compareUserChoice(userChoice);
-});
-const bankButton= document.querySelector("#bank-btn");
-bankButton.addEventListener("click", () => {
-  bankPoints();
-});
-
-// Compare choice
-let bankedPointsElement = document.querySelector("#banked-pts");
-let streakPointsElement = document.querySelector("#streak-pts");
-let streakPoints = parseInt(streakPointsElement.innerHTML);
-let bankedPoints = parseInt(bankedPointsElement.innerHTML);
-
-function compareUserChoice(userChoice) {
-  if (userChoice === "Higher") {
-    if (currentCardValue >= previousCardValue) {
-      if (streakPoints > 1) {
-        streakPoints += streakPoints;
-      } else {
-        streakPoints += 1;
+      for (const suit of suits) {
+        for (const value of values) {
+          const card = {
+            name: `${value}_of_${suit}`,
+            value: values.indexOf(value) + 1, // Assign values from 1 to 13
+          };
+          cards.push(card);
+        }
       }
-      streakPointsElement.innerHTML = streakPoints;
-    } else {
-      loseStreak();
     }
-  } else if (userChoice === "Lower") {
-    if (currentCardValue <= previousCardValue) {
-      if (streakPoints > 1) {
-        streakPoints += streakPoints;
-      } else {
-        streakPoints += 1;
+
+    function initializeGame() {
+      createDeck();
+      drawCard();
+    }
+
+    /**
+     * Displays a random card from the cards array in the card-container div
+     *
+     */
+    function drawCard() {
+      previousCardValue = currentCardValue;
+      const randomIndex = Math.floor(Math.random() * cards.length);
+      const selectedCard = cards[randomIndex];
+
+      // Create the filename for the card image
+      const cardImageName = `${selectedCard.name}.svg`;
+
+      // Set the source of the current card element to display the selected card
+      currentCard.src = `assets/images/cards/SVG-cards-1.3/${cardImageName}`;
+      currentCard.alt = `${cardImageName}`;
+      currentCard.setAttribute("aria-label", cardImageName);
+      currentCardValue = selectedCard.value;
+    }
+
+    const higher = document.querySelector("#higher-btn");
+    higher.addEventListener("click", () => {
+      drawCard();
+      userChoice = "Higher";
+      compareUserChoice(userChoice);
+    });
+
+    lower.addEventListener("click", () => {
+      drawCard();
+      userChoice = "Lower";
+      compareUserChoice(userChoice);
+    });
+
+    bankButton.addEventListener("click", () => {
+      bankPoints();
+    });
+
+    function compareUserChoice(userChoice) {
+      if (userChoice === "Higher") {
+        if (currentCardValue >= previousCardValue) {
+          if (streakPoints > 1) {
+            streakPoints += streakPoints;
+          } else {
+            streakPoints += 1;
+          }
+          streakPointsElement.innerHTML = streakPoints;
+        } else {
+          loseStreak();
+        }
+      } else if (userChoice === "Lower") {
+        if (currentCardValue <= previousCardValue) {
+          if (streakPoints > 1) {
+            streakPoints += streakPoints;
+          } else {
+            streakPoints += 1;
+          }
+          streakPointsElement.innerHTML = streakPoints;
+        } else {
+          loseStreak();
+        }
       }
-      streakPointsElement.innerHTML = streakPoints;
-    } else {
-      loseStreak();
+    }
+
+    function loseStreak() {
+      streakPoints = 0;
+      streakPointsElement.innerHTML = 0;
+      createDeck();
+    }
+
+    function winCondition() {
+      let bankedPoints = parseInt(
+        document.querySelector("#banked-pts").innerHTML
+      );
+      if (bankedPoints >= 50) {
+        winnerModal.showModal();
+      }
+    }
+
+    function bankPoints() {
+      bankedPoints += streakPoints; // Add streak points to banked points
+      streakPoints = 0; // Reset streak to 0
+      streakPointsElement.innerHTML = streakPoints; // Update streak points display
+      bankedPointsElement.innerHTML = bankedPoints; // Update banked points display
+      initializeGame();
+      winCondition();
     }
   }
-}
-
-function loseStreak() {
-  streakPoints = 0;
-  streakPointsElement.innerHTML = 0;
-  createDeck();
-}
-
-function winCondition() {
-  let bankedPoints = parseInt(document.querySelector("#banked-pts").innerHTML);
-  if (bankedPoints >= 30) {
-    winnerModal.showModal();
-  }
-}
-
-function bankPoints() {
-  bankedPoints += streakPoints; // Add streak points to banked points
-  streakPoints = 0; // Reset streak to 0
-  streakPointsElement.innerHTML = streakPoints; // Update streak points display
-  bankedPointsElement.innerHTML = bankedPoints; // Update banked points display
-  initializeGame();
-  winCondition();
-}
+});
